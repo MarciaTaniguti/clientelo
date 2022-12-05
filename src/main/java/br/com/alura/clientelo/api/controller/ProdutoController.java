0e33f9ca4457b900e8198c9274ad766e1c941503
Marcia Taniguti <MarciaTaniguti>
2022-12-05 03:49:31 -0300

    [semana9-marcia] [Teste] Adição do CacheEvict quando é cadastrado um novo produto.

--- src/main/java/br/com/alura/clientelo/api/controller/ProdutoController.java
+++ src/main/java/br/com/alura/clientelo/api/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import br.com.alura.clientelo.api.form.CadastroProdutoForm;
 import br.com.alura.clientelo.api.form.ExibeProdutoForm;
 import br.com.alura.clientelo.service.CrudProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,6 +13,7 @@ import org.springframework.data.web.SortDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -23,6 +25,8 @@ public class ProdutoController {
 	private CrudProdutoService produtoService;
 
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	@Transactional
+	@CacheEvict(value = "listaProdutos", allEntries = true)
 	public ResponseEntity<CadastroProdutoForm> cadastrar(@RequestBody @Valid CadastroProdutoForm produto) {
 		CadastroProdutoForm produtoCriado = produtoService.cadastra(produto);
 		return ResponseEntity.status(HttpStatus.CREATED).body(produtoCriado);
